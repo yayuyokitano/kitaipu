a58ba5e860081c4babc41a2335cd0bb4e98e7ed1
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2022-03-05 15:26:38 +0800

    omitempty

--- interaction.go
+++ interaction.go
@@ -223,7 +223,7 @@ type InteractionResponseMessage struct {
 	TTS             bool                `json:"tts,omitempty"`
 	Content         string              `json:"content,omitempty"`
 	Embeds          []Embed             `json:"embeds,omitempty"`
-	AllowedMentions AllowedMentions     `json:"allowed_mentions,omitempty"`
+	AllowedMentions *AllowedMentions    `json:"allowed_mentions,omitempty"`
 	Flags           MessageFlags        `json:"flags,omitempty"`
 	Components      []Component         `json:"components,omitempty"`
 	Attachments     []PartialAttachment `json:"attachments,omitempty"`
